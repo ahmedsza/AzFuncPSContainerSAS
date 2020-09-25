7623b2de195823695424ec88852894be6d0a30b4
ahmedsza <37135205+ahmedsza@users.noreply.github.com>
2020-09-25 13:16:10 +0000

    revert to original code - closes #1

--- AzFuncSAS/HttpSASDemo/run.ps1
+++ AzFuncSAS/HttpSASDemo/run.ps1
@@ -71,7 +71,7 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 #}
 
 $body=$returnresult
-$body="test"
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
