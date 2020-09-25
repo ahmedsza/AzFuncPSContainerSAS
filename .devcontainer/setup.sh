01db7cd4171e1e788be7c37ce1abc20388c8db1e
ahmeds <ahmeds@ms.com>
2020-09-25 14:14:14 +0200

    add az cli to devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,7 +13,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 ## Install Docker CE
 curl -fsSL https://get.docker.com | bash
-
+curl -sL https://aka.ms/InstallAzureCLIDeb |  bash
 ## Update to the latest PowerShell
 curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
